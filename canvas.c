51e42e49e8bd006c3c5901dbdc673e8bae886998
Dianna Barry <diannajbarry@gmail.com>
2018-04-30 19:41:43 -0400

    c function name changes, minor specification changes to canvas functions

--- canvas.c
+++ canvas.c
@@ -53,56 +53,97 @@ struct Circles{
 struct Drawing {
   TagType type;
   void* value;
+  struct Drawing* next;
 };
 
-static struct Drawing drawings[100];
-static int counter = 0;
+static struct Drawing* drawings = NULL;
+static struct Drawing* last = NULL;
+static char canvasSize[100];
+
+
+void insertEnd(void* nextNode, TagType type){
+  //create new node and assign data
+  struct Drawing* draw = (struct Drawing*) malloc(sizeof(struct Drawing));
+  if( draw == NULL){
+    printf("Error: Out of Memory");
+    exit(1);
+  }
+  draw->type = type;
+  draw->value = nextNode;
+  draw->next = NULL;
+
+  if(drawings == NULL){
+    //empty list
+    drawings = draw;
+    last = draw;
+  } else {
+    if(last != NULL){ 
+      last->next = draw;
+      last = draw;
+    }
+  }
+} 
+
+void clearList(){
+  struct Drawing* node = drawings; 
+  struct Drawing* next;
+  while(node != NULL){
+    next = node->next;
+    free(node->value);
+    free(node);
+    node = next;
+  }
+  drawings = NULL;
+  last = NULL;
+}
+
 int redraw_cb( Ihandle *self, float x, float y )
 {
   cdCanvasActivate(cdcanvas);
   cdCanvasClear(cdcanvas);
   cdCanvasForeground(cdcanvas, CD_RED);
 
-  if(counter > 0){
-    for(int i = 0; i < counter; i++){
-      switch(drawings[i].type){
-        case 1: ;
-          struct Lines *line = drawings[i].value;  
-          cdCanvasLine(cdcanvas, line->x1, line->y1, line->x2, line->y2);
-          break;
-        case 0: ;
-          struct Circles *circle = drawings[i].value;
-          if(circle->fill == 0)
-            cdCanvasArc(cdcanvas, circle->x1, circle->y1, circle->x2, circle->y2, circle->angle1, circle->angle2);
-          else
-            cdCanvasSector(cdcanvas, circle->x1, circle->y1, circle->x2, circle->y2, circle->angle1,circle->angle2);
-          break;
-        case 2: ;
-          struct Rects *rect = drawings[i].value;
-          if(rect->fill == 0){
-            cdCanvasRect(cdcanvas, rect->x1, rect->x2, rect->y1, rect->y2);
-          } else {
-            cdCanvasBox(cdcanvas, rect->x1, rect->x2, rect->y1, rect->y2);
-          }
-          break;
-        case 3: ;
-          struct Triangles *tri = drawings[i].value;
-          cdCanvasBegin(cdcanvas, CD_CLOSED_LINES);
-          cdCanvasVertex(cdcanvas, tri->x1, tri->y1);
-          cdCanvasVertex(cdcanvas, tri->x2, tri->y2);
-          cdCanvasVertex(cdcanvas, tri->x3, tri->y3);
-          cdCanvasEnd(cdcanvas);
-      }  
+  struct Drawing* node = drawings;
+  while(node != NULL){
+    switch(node->type){
+      case 1: ;
+        struct Lines *line = node->value;  
+        cdCanvasLine(cdcanvas, line->x1, line->y1, line->x2, line->y2);
+        break;
+      case 0: ;
+        struct Circles *circle = node->value;
+        if(circle->fill == 0)
+          cdCanvasArc(cdcanvas, circle->x1, circle->y1, circle->x2, circle->y2, circle->angle1, circle->angle2);
+        else
+          cdCanvasSector(cdcanvas, circle->x1, circle->y1, circle->x2, circle->y2, circle->angle1,circle->angle2);
+        break;
+      case 2: ;
+        struct Rects *rect = node->value;
+        if(rect->fill == 0){
+          cdCanvasRect(cdcanvas, rect->x1, rect->x2, rect->y1, rect->y2);
+        } else {
+          cdCanvasBox(cdcanvas, rect->x1, rect->x2, rect->y1, rect->y2);
+        }
+        break;
+      case 3: ;
+        struct Triangles *tri = node->value;
+        cdCanvasBegin(cdcanvas, CD_CLOSED_LINES);
+        cdCanvasVertex(cdcanvas, tri->x1, tri->y1);
+        cdCanvasVertex(cdcanvas, tri->x2, tri->y2);
+        cdCanvasVertex(cdcanvas, tri->x3, tri->y3);
+        cdCanvasEnd(cdcanvas);
     }
-/*    for(int j =0; j < counter; j++){
-      free(drawings[j].value);
-      drawings[j].value = NULL;
-      drawings[j].type = -1;
-    }
-    counter = 0;*/
+    node = node->next;  
   }
+  //clearList();
   return IUP_DEFAULT;
 }
+
+c_clearCanvas(){
+  clearList();
+  return PICAT_TRUE;
+}
+
 
 int getFill(int fill){
   if( fill > 0)
@@ -113,48 +154,25 @@ int getFill(int fill){
 
 int assignShape(int x1, int x2, int y1, int y2, int fill, TagType type){
   //Puts the variables into array holding shapes we want to draw
-  if(counter < 100){
-    switch (type){
-      case 0: //Circle
-        drawings[counter].type = type;
-        struct Circles *circle;
-        circle = (struct Circles*) malloc(sizeof(struct Circles));
-        if(circle == NULL){
-          printf("Error: Out of Memory\n";
-          exit(1);
-        }
-        circle->x1 = x1;
-        circle->x2 = x2;
-        circle->y1 = y1;
-        circle->y2 = y2;
-        circle->angle1 = 0;
-        circle->angle2 = 360;
-        circle->fill = getFill(fill);
-        drawings[counter].value = circle;
-        break;
-      case 2: //Rectangle
-       drawings[counter].type = type;
-        struct Rects *rect;
-        rect = (struct Rects*) malloc(sizeof(struct Rects));
-        if(rect == NULL){
-          printf("Error: Out of Memory\n";
-          exit(1);
-        }
-        rect->x1 = x1;
-        rect->x2 = x2;
-        rect->y1 = y1;
-        rect->y2 = y2;
-        rect->fill = getFill(fill);
-        drawings[counter].value = rect;
-        break;
+  switch (type){
+    case 2: ;//Rectangle
+      struct Rects *rect;
+      rect = (struct Rects*) malloc(sizeof(struct Rects));
+      if(rect == NULL){
+        printf("Error: Out of Memory\n");
+        exit(1);
       }
-    counter++;
+      rect->x1 = x1;
+      rect->x2 = x2;
+      rect->y1 = y1;
+      rect->y2 = y2;
+      rect->fill = getFill(fill);
+      insertEnd(rect, type);
+      break;
   }
 }
 
 int assignLine(int x1, int x2, int y1, int y2, TagType type){
-  if(counter < 100){
-    drawings[counter].type = type;
     if(type == 1){
       struct Lines *line;
       line = (struct Lines*) malloc(sizeof(struct Lines));
@@ -166,15 +184,12 @@ int assignLine(int x1, int x2, int y1, int y2, TagType type){
       line->x2 = x2;
       line->y1 = y1;
       line->y2 = y2;
-      drawings[counter].value = line;  
+      insertEnd(line, type);
     } 
-    counter++;
-  }
 }
 
 int assignArc(int x1, int x2, int y1, int y2, long angle1, long angle2, int fill, TagType type){
-  if(counter < 100 && type == 0){
-    drawings[counter].type = type;
+  if( type == 0){
     struct Circles *circle;
     circle = (struct Circles*) malloc(sizeof(struct Circles));
     if(circle == NULL){
@@ -188,14 +203,12 @@ int assignArc(int x1, int x2, int y1, int y2, long angle1, long angle2, int fill
     circle->angle1 = angle1;
     circle->angle2 = angle2;
     circle->fill = getFill(fill);
-    drawings[counter].value = circle; 
-    counter++;
+    insertEnd(circle, type); 
   }
 }
 
 int assignTri(int x1, int y1, int x2, int y2, int x3, int y3, int fill, TagType type){
-  if(counter < 100 && type == 3){
-    drawings[counter].type = type;
+  if(type == 3){
     struct Triangles* tri;
     tri = (struct Triangles*) malloc(sizeof(struct Triangles));
     if(tri == NULL){
@@ -208,11 +221,11 @@ int assignTri(int x1, int y1, int x2, int y2, int x3, int y3, int fill, TagType 
     tri->y2 = y2;
     tri->x3 = x3;
     tri->y3 = y3;
-    drawings[counter].value = tri; 
-    counter++;
+    insertEnd(tri, type);
   }
 }
-arc(){
+
+c_arc(){
   TERM x1 	= picat_get_call_arg(1,7);
   TERM y1 	= picat_get_call_arg(2,7);
   TERM x2 	= picat_get_call_arg(3,7);
@@ -251,11 +264,11 @@ arc(){
   if(exception == NULL){
     return PICAT_FALSE;
   }
-  assignArc((int)cX1,(int) cX2,(int) cY1, (int)cY2, cAngle1, cAngle2, (int)cFill, Circle); 
+  assignArc((int)cX1+cX2/2,(int) cX2,(int) cY1-cY2/2, (int)cY2, cAngle1, cAngle2, (int)cFill, Circle); 
   return PICAT_TRUE;
 }
 
-triangle(){
+c_triangle(){
   TERM x1 	= picat_get_call_arg(1,7);
   TERM y1 	= picat_get_call_arg(2,7);
   TERM x2 	= picat_get_call_arg(3,7);
@@ -297,7 +310,8 @@ triangle(){
   assignTri((int)cX1,(int) cY1,(int) cX2, (int)cY2, (int) cX3,(int) cY3, (int)cFill, Triangle); 
   return PICAT_TRUE;
 }
-oval(){
+
+c_oval(){
   TERM x1 = picat_get_call_arg(1,5);
   TERM y1 = picat_get_call_arg(2,5);
   TERM x2 = picat_get_call_arg(3,5);
@@ -326,11 +340,11 @@ oval(){
   if(exception == NULL){
     return PICAT_FALSE;
   }
-  assignShape((int)cX1,(int) cX2,(int) cY1, (int)cY2, (int)cFill, Circle); 
+  assignArc((int)cX1 + cX2/2,(int) cX2,(int) cY1 - cY2/2, (int)cY2, 0, 360, (int)cFill, Circle); 
   return PICAT_TRUE;
 }
 
-circle(){
+c_circle(){
   TERM x1 = picat_get_call_arg(1,4);
   TERM y1 = picat_get_call_arg(2,4);
   TERM x2 = picat_get_call_arg(3,4);
@@ -354,11 +368,11 @@ circle(){
   if(exception == NULL){
     return PICAT_FALSE;
   }
-  assignShape((int)cX1,(int) cX2,(int) cY1, (int)cX2, (int)cFill, Circle); 
+  assignArc((int)cX1+ cX2/2,(int) cX2,(int) cY1 - cX2/2, (int)cX2, (int)cFill, 0, 360, Circle); 
   return PICAT_TRUE;
 }
 
-line(){
+c_line(){
   TERM x1 = picat_get_call_arg(1,4);
   TERM y1 = picat_get_call_arg(2,4);
   TERM x2 = picat_get_call_arg(3,4);
@@ -386,10 +400,10 @@ line(){
   return PICAT_TRUE;
 }
 
-rectangle(){//need to separate out filled and unfilled rectangles(Box vs Rect)
+c_rectangle(){
   TERM xMin = picat_get_call_arg(1,5);
-  TERM xMax = picat_get_call_arg(2,5);
-  TERM yMin = picat_get_call_arg(3,5);
+  TERM yMin = picat_get_call_arg(2,5);
+  TERM xMax = picat_get_call_arg(3,5);
   TERM yMax = picat_get_call_arg(4,5);
   TERM fill = picat_get_call_arg(5,5);
   if(!picat_is_integer(xMin) || !picat_is_integer(xMax) || !picat_is_integer(yMin) ||!picat_is_integer(yMax) || !picat_is_integer(fill)){
@@ -415,26 +429,46 @@ rectangle(){//need to separate out filled and unfilled rectangles(Box vs Rect)
   if(exception == NULL){
     return PICAT_FALSE;
   }
-  assignShape((int)cXMin,(int) cXMax,(int) cYMin, (int)cYMax, (int)cFill, Rect); 
+  assignShape((int)cXMin,(int) cXMax+ cXMin,(int) cYMin, (int)cYMax+ cYMin, (int)cFill, Rect); 
   return PICAT_TRUE;
 }
 
+c_star(){
+  TERM n = picat_get_call_arg(1,6);
+  TERM x = picat_get_call_arg(2,6);
+  TERM y = picat_get_call_arg(3,6);
+  TERM angle = picat_get_call_arg(4,6);
+  TERM diameter = picat_get_call_arg(5,6);
+  TERM fill = picat_get_call_arg(6,6);
 
-canvas()
+}
+
+
+c_canvas()
 {
+  TERM x = picat_get_call_arg(1,2);
+  TERM y = picat_get_call_arg(2,2);
+
+
+  if(!picat_is_integer(x) || !picat_is_integer(y)){
+    return PICAT_FALSE;
+  }
+  long cX = picat_get_integer(x);
+  long cY = picat_get_integer(y);
+
+  snprintf(canvasSize, 100, "%lix%li", cX, cY);
   Ihandle *dlg, *cnvs;
   IupOpen(NULL, NULL);
 
   cnvs = IupCanvas( NULL );
   IupSetCallback(cnvs, "ACTION",( Icallback )redraw_cb );
-  IupSetAttribute(cnvs, "SIZE", "300x100");
+  IupSetAttribute(cnvs, "SIZE", canvasSize);
 
   dlg = IupDialog( IupVbox( cnvs, NULL ) );
   IupSetAttribute(dlg, "TITLE", "IupCanvas + Canvas Draw" );
   IupSetAttribute(dlg, "MARGIN", "10x10");
   IupMap( dlg );
 
-  printf("Counter at %d\n", counter);
   //cdInitContextPlus();
   //cdUseContextPlus(1);
 
